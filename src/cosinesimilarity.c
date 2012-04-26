b7333efec11b31c39da517478c48e419ad754a2e
James Moore <moore.work@live.com>
2012-04-26 12:44:56 -0400

    modified:   bin/cosinesimilarity.o 	modified:   bin/window.o 	modified:   src/cosinesimilarity.c

--- src/cosinesimilarity.c
+++ src/cosinesimilarity.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 #include <cv.h>
+#include <stdio.h>
 #include "cosinesimilarity.h"
 #define DEBUG 1
 
 //CvScalar cosine_similarity(CvMat test, CvMat patch);
 //int Resemblance(CvScalar map);
+
+
+void mmm(char * t, char * q, char * dest, int N);
 
 //Do the cosine similarity operation
 //See: Frobenius inner product
@@ -16,7 +20,7 @@ CvScalar cosine_similarity(CvMat * test, CvMat * patch)
 
 
 	//Take the product of the two matrices 
-	cvMatMul(test,patch, dest);	
+	mmm(test->data.ptr,test->data.ptr,dest->data.ptr,patch->cols);
 
 	//Trace the matrices 
 	CvScalar scale = cvTrace(dest); 
@@ -24,17 +28,31 @@ CvScalar cosine_similarity(CvMat * test, CvMat * patch)
 }
 
 //int?
-double Resemblance(CvScalar map){
+double Resemblance(CvScalar map)
+{
 	double result = (int)(map.val[0] * map.val[0])/(1-(map.val[0]*map.val[0]));
 	return result;
 } 
 
-void significance(resembleList res){
+void significance(resembleList res)
+{
 	int i;
 
 	for(i = 0; i < res.size;i++){
 		if(res.resemblences[i] >= TOLERANCE){
 			res.resemblences[i] = 0.0;		
 		}
-	} 
+	}
 }
+void mmm(char * t, char * q, char * dest, int N)
+{
+	int i,k,j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			for (k = 0; k < N; k++) {
+				dest[i * N + j] += t[i * N + k] * q[k * N +j];
+			}   
+		}   
+	} 
+} 
+
